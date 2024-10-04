377d03a6548eaffdefe67db199c90589a6acbd61
William Ballesteros <william.ballesteros@energage.com>
2024-10-04 11:27:39 -0500

    Create an example of custom hook to fech server data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from '@chakra-ui/react'
-
-interface Game {
-    id: number;
-    name: string;
-}
-
-interface FetchGamesResponse {
-    count: number;
-    results: Game[]
-}
-
+import useGames from '../hooks/useGames'
 
 const GameGrid = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-    
-    useEffect(() => {
-        apiClient.get<FetchGamesResponse>('/xgames')
-        .then(res => {
-            debugger;
-            console.log(res.data.results);
-            setGames(res.data.results)}
-        )
-        .catch(err => setError(err.message));
-    })
+    const {games, error } = useGames();
 
     return (
         <>
