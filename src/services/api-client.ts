f78fe74c0e8ae8341fa1f6d26a1225e5349bbc1d
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-10-09 16:09:30 -0400

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api/games',
     params: {
-        key: 'b66880cc2f79403ab7684e3593cfe0a5'
+        key: process.env.REACT_APP_API_KEY
     }
-})
+});
